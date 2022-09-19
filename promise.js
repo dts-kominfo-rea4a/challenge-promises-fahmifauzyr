9badca3e79f267aa697806a2af0077904651a703
fahmifauzyr <fahmifauzyffr20@gmail.com>
2022-09-19 21:08:26 +0700

    Penambahan resolve

--- promise.js
+++ promise.js
@@ -1,7 +1,31 @@
 const { promiseTheaterIXX, promiseTheaterVGC } = require("./external.js");
 
 // TODO: Buat fungsi promiseOutput sesuai ketentuan readme
-const promiseOutput = null;
+//const promiseOutput = null;
+
+const promiseOutput = (emosi) => {
+  let hitung = 0;
+
+  return new Promise(async (resolve) => {
+    let theaterIIX = await promiseTheaterIXX().then((data) => {
+      data.forEach((element) => {
+        if (element.hasil === emosi) {
+          hitung++;
+        }
+      });
+    });
+
+    let theaterVGC = await promiseTheaterVGC().then((data) => {
+      data.forEach((element) => {
+        if (element.hasil === emosi) {
+          hitung++;
+        }
+      });
+    });
+
+    resolve(hitung);
+  });
+};
 
 module.exports = {
   promiseOutput,
